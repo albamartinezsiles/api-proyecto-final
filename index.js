f8488e92da99d253dbd70a03640ba6ed0b9916f7
Alba <dok-do@hotmail.es>
2024-04-28 13:36:24 +0200

    final

--- index.js
+++ index.js
@@ -5,20 +5,16 @@ const { MongoClient, ObjectId } = require("mongodb");
 const servidor = express();
 const bodyParser = require('body-parser'); //permite extraer la información del cuerpo de la petición (para post, put, delete, etc)
 
-servidor.use(cors());
+servidor.use(cors()); //si no usamos esto no podemos hacer peticiones desde el front, porque no permite que se hagan peticiones desde otros dominios
 servidor.use(bodyParser.json()); //extrae la información y crea el objeto body en la petición
 
+/*servidor.use("/prueba",express.static("./index.html")); */ //esto es para hacer pruebas de la api de forma sencilla (comprobar peticiones post, delete, etc)
 
-
-
-servidor.use("/prueba",express.static("./index.html"));
-
-function conectar(){
-    return MongoClient.connect(process.env.URL_MONGO);
+function conectar(){ //conectar a la base de datos. Lo ponemos en una función para invocarla y no tener que reescribir el código
+    return MongoClient.connect(process.env.URL_MONGO); //usamos process.env para ocultar la url de la bd
 }
 
 servidor.get("/tareas", async (peticion,respuesta) => {
-
     const conexion = await conectar();
 
     let coleccion = conexion.db("tareas").collection("tareas")
@@ -29,30 +25,30 @@ servidor.get("/tareas", async (peticion,respuesta) => {
     respuesta.json(tareas);
     conexion.close();
     
-    //esto tarda mucho en cargar no se por qué pero me da el console.log
+    //En un principio esto me tardaba mucho en cargar pero me daba el console.log. Creo que era problema de la bd. También me dio problemas a la hora de subirlo a render porque no me cargaba las tareas (no había puesto que la bd pudiera recibir peticiones desde cualquier ip)
 
 });
 
 servidor.post("/crear-tarea", async (peticion, respuesta) => {
     const conexion = await conectar();
-    const nuevaTarea = peticion.body;
+    const nuevaTarea = peticion.body; //recojo la tarea y la meto en una constante para poder trabajar con ella
     console.log(nuevaTarea);
 
     let coleccion = conexion.db("tareas").collection("tareas");
 
     try {
         if (!nuevaTarea.textoTarea || nuevaTarea.textoTarea.trim() === '') {
-            respuesta.status(400).send("La tarea tiene que tener un texto");
+            respuesta.send("La tarea tiene que tener un texto"); //si no hay texto en la tarea, devuelvo un error
         } else {
-            coleccion.insertOne({ textoTarea: nuevaTarea.textoTarea, editando: false, estado: "pendiente"})
+            coleccion.insertOne({ textoTarea: nuevaTarea.textoTarea, editando: false, estado: "pendiente"}) //inserto la tarea en la bd
             .then(resultado => {
                 conexion.close();
-                respuesta.json({ tarea: nuevaTarea});
+                respuesta.json({ tarea: nuevaTarea}); //devuelvo la tarea creada en formato json
                 console.log("Tarea creada correctamente");
             });
         }
     } catch (error) {
-        console.error("Error al crear la tarea:", error);
+        console.error("Error al crear la tarea:");
         respuesta.send("Error interno al crear la tarea");
     }
 });
@@ -62,14 +58,14 @@ servidor.delete("/tareas/borrar/:id", async (peticion,respuesta) => {
     const conexion = await conectar();
     let coleccion = conexion.db("tareas").collection("tareas")
 
-    let resultado = await coleccion.deleteOne({ _id : new ObjectId(peticion.params.id) });
+    let resultado = await coleccion.deleteOne({ _id : new ObjectId(peticion.params.id) }); //borro la tarea con el id que me llega en la petición
     console.log(resultado);
     conexion.close();
 
-    if (resultado.deletedCount === 1) {
+    if (resultado.deletedCount === 1) { //si se ha borrado una tarea, devuelvo un mensaje de confirmación que luego podremos manipular en el front
         respuesta.json({ tareaborrada : "ok"});
     } else {
-        respuesta.status(404).send("Error al borrar la tarea");
+        respuesta.status(404).send("Error al borrar la tarea"); //si no se ha borrado, devuelvo un error
     }
 });
 
@@ -78,16 +74,16 @@ servidor.put("/tareas/editar/:id", async (peticion,respuesta) => {
         const conexion = await conectar();
         let coleccion = conexion.db("tareas").collection("tareas")
     
-        //editar
+        //editar el texto de la tarea
 
         let resultado = await coleccion.updateOne({ _id : new ObjectId(peticion.params.id) }, { $set: { textoTarea: peticion.body.textoTarea } });
         console.log(resultado);
         conexion.close();
     
         if (resultado.modifiedCount === 1) {
-            respuesta.status(200).send("Tarea editada correctamente");
+            respuesta.status(200).send("Tarea editada correctamente"); //si se ha editado la tarea, devuelvo un mensaje de confirmación. Lo manipularemos en el front con el status
         } else {
-            respuesta.status(404).send("Error al editar la tarea");
+            respuesta.status(404).send("Error al editar la tarea"); //si no se ha editado, es decir, el texto de la tarea está igual, devuelvo un error
         }
     });
 
@@ -98,27 +94,27 @@ servidor.put("/tareas/editar/:id", async (peticion,respuesta) => {
         const nuevoEstado = peticion.body.estado;
         const idTarea = new ObjectId(peticion.params.id);
     
-        const resultado = await coleccion.updateOne({ _id: idTarea }, { $set: { estado: nuevoEstado } });
-        console.log(peticion.body.estado);
+        const resultado = await coleccion.updateOne({ _id: idTarea }, { $set: { estado: nuevoEstado } }); //guardo el id de la tarea y el nuevo estado en una constante
+        console.log(peticion.body.estado); //compruebo que el estado se cambia correctamente
         conexion.close();
       
         if (resultado.modifiedCount === 1) {
-            respuesta.json({ estadoModificado: true });
+            respuesta.json({ estadoModificado: true }); //si se ha modificado el estado de la tarea, devuelvo un json con estadoModificado a true
         } else {
-            respuesta.status(404).send("Error al actualizar el estado de la tarea");
+            respuesta.status(404).send("Error al actualizar el estado de la tarea"); //si no se ha modificado, devuelvo un error
         }
     });
 
 //errores
-servidor.use((peticion,respuesta) => { //cualquier cosa que no encaje va a error not found!
+servidor.use((peticion,respuesta) => { //cualquier petición que no encaje va a error not found
     respuesta.status(404);
     respuesta.json({ error : "not found" });
 });
 
 
-servidor.use((error,peticion,respuesta,siguiente) => { //a este middleware se llega cuando se invoca el siguiente con un error
+servidor.use((error,peticion,respuesta,siguiente) => { //a este middleware se llega cuando se invoca siguiente con un error
     respuesta.status(400);
     respuesta.json({ error : "petición no válida" });
 });
 
-servidor.listen(process.env.PORT || 4000)
+servidor.listen(process.env.PORT || 4000) 
